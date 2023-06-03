57ce0be15810755ea1cfe9ccef578c4effb741db
LielVaknin <73184238+LielVaknin@users.noreply.github.com>
2023-06-03 22:24:43 +0300

    First commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+using Fusion;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Health : NetworkBehaviour
+{
+    [SerializeField] NumberField HealthDisplay;
+    [SerializeField] NumberField ScoreDisplay;
+    public bool hasShield = false;
+
+    [Networked(OnChanged = nameof(NetworkedHealthChanged))]
+    public int NetworkedHealth { get; set; } = 100;
+
+    [Networked(OnChanged = nameof(NetworkedScoreChanged))]
+    public int NetworkedScore { get; set; } = 0;
+
+    private static void NetworkedHealthChanged(Changed<Health> changed)
+    {
+        Debug.Log($"Health changed to: {changed.Behaviour.NetworkedHealth}");
+        changed.Behaviour.HealthDisplay.SetNumber(changed.Behaviour.NetworkedHealth);
+    }
+
+    private static void NetworkedScoreChanged(Changed<Health> changed)
+    {
+        Debug.Log($"Score changed to: {changed.Behaviour.NetworkedScore}");
+        changed.Behaviour.ScoreDisplay.SetNumber(changed.Behaviour.NetworkedScore);
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void DealDamageRpc(int damage)
+    {
+        if (hasShield) return; // Player has a shield, ignore the damage
+
+        NetworkedHealth -= damage;
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void AddScoreRpc(int score)
+    {
+        NetworkedScore += score;
+    }
+
+    public void CollectShield()
+    {
+        hasShield = true;
+    }
+}
---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    private bool isCollected = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isCollected) return; // Shield already collected
+
+        var player = other.GetComponent<Health>();
+        if (player != null)
+        {
+            player.CollectShield();
+            isCollected = true;
+            Destroy(gameObject); // Destroy the shield object
+        }
+    }
+}
