36eff52058a871ae3ad7d83dfe2f46b48dae7e53
LielVaknin <73184238+LielVaknin@users.noreply.github.com>
2023-06-07 12:54:40 +0300

    Update PlayerSpawner

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -1,14 +1,3 @@
-// using Fusion;
-// using UnityEngine;
-
-// public class PlayerSpawner : SimulationBehaviour, IPlayerJoined {
-//     public GameObject PlayerPrefab;
-//     public void PlayerJoined(PlayerRef player) {
-//         if (player == Runner.LocalPlayer) {
-//             Runner.Spawn(PlayerPrefab, new Vector3(0, 1, 0), Quaternion.identity, player);
-//         }
-//     }
-// }
 using Fusion;
 using UnityEngine;
 
@@ -22,7 +11,7 @@ public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
         {
             NetworkObject spawnedPlayerObject = Runner.Spawn(PlayerPrefab, new Vector3(0, 1, 0), Quaternion.identity, player);
             GameObject spawnedPlayer = spawnedPlayerObject.gameObject;
-            spawnedPlayer.transform.rotation = Quaternion.Euler(-90, 0, 0); // Set the desired rotation here
+            spawnedPlayer.transform.rotation = Quaternion.Euler(-90, 0, 0); 
         }
     }
 }
